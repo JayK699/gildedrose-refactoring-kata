bb1d190068272217ac821ca6260ef7e58935551e
JayK669 <kkudanowski@gmail.com>
2022-12-08 13:49:54 +0100

    Made more simple and merged an "if"

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -11,38 +11,23 @@ void GildedRose::updateQuality()
 {
     for(int i = 0; i < items.size(); i++)
     {
-        if(items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-        {
-            if(items[i].quality > 0)
-            {
-                if(items[i].name != "Sulfuras, Hand of Ragnaros")
-                {
-                    items[i].quality--;
-                }
-            }
+        if(items[i].quality > 0 && items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" && items[i].name != "Sulfuras, Hand of Ragnaros")
+        {                        
+                    items[i].quality--;                          
         }
         else
         {
-            if(items[i].quality < 50)
+            if(items[i].quality < 50 && items[i].quality != 0)
             {
                 items[i].quality++;
 
-                if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if(items[i].sellIn < 11)
-                    {
-                        if(items[i].quality < 50)
-                        {
-                            items[i].quality++;
-                        }
-                    }
+                if (items[i].name == "Backstage passes to a TAFKAL80ETC concert" && items[i].sellIn < 11 && items[i].quality < 50)
+                {                                      
+                    items[i].quality++;     
 
-                    if(items[i].sellIn < 6)
-                    {
-                        if (items[i].quality < 50)
-                        {
-                            items[i].quality++;
-                        }
+                    if(items[i].sellIn < 6 && items[i].quality < 50)
+                    {                      
+                            items[i].quality++;                       
                     }
                 }
             }
@@ -53,32 +38,21 @@ void GildedRose::updateQuality()
             items[i].sellIn--;
         }
 
-        if(items[i].sellIn < 0)
+        if(items[i].sellIn < 0 && items[i].name != "Aged Brie")
         {
-            if(items[i].name != "Aged Brie")
-            {
-                if(items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if(items[i].quality > 0)
-                    {
-                        if(items[i].name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            items[i].quality--;
-                        }
-                    }
-                }
-                else
-                {
-                    items[i].quality -= items[i].quality;
-                }
+            
+            if(items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+            {                   
+                items[i].quality--;   
             }
             else
             {
-                if(items[i].quality < 50)
-                {
-                    items[i].quality++;
-                }
-            }
+                items[i].quality -= items[i].quality;
+            }           
         }
+        else if(items[i].sellIn < 0 && items[i].quality < 50)
+            {  
+                    items[i].quality++;                
+            }
     }
 }
