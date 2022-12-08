b569f57138a87ff061d7d7734f08aff666daf912
JayK669 <kkudanowski@gmail.com>
2022-12-08 12:23:48 +0100

    use operators of decrease and increase

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -9,11 +9,11 @@ GildedRose::GildedRose(::std::vector<Item> && items) : items(::std::move(items))
 
 void GildedRose::updateQuality()
 {
-    for (int i = 0; i < items.size(); i++)
+    for(int i = 0; i < items.size(); i++)
     {
-        if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+        if(items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert")
         {
-            if (items[i].quality > 0)
+            if(items[i].quality > 0)
             {
                 if (items[i].name != "Sulfuras, Hand of Ragnaros")
                 {
@@ -23,23 +23,23 @@ void GildedRose::updateQuality()
         }
         else
         {
-            if (items[i].quality < 50)
+            if(items[i].quality < 50)
             {
                 items[i].quality++;
 
-                if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
+                if(items[i].name == "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    if (items[i].sellIn < 11)
+                    if(items[i].sellIn < 11)
                     {
-                        if (items[i].quality < 50)
+                        if(items[i].quality < 50)
                         {
                             items[i].quality++;
                         }
                     }
 
-                    if (items[i].sellIn < 6)
+                    if(items[i].sellIn < 6)
                     {
-                        if (items[i].quality < 50)
+                        if(items[i].quality < 50)
                         {
                             items[i].quality++;
                         }
@@ -48,20 +48,20 @@ void GildedRose::updateQuality()
             }
         }
 
-        if (items[i].name != "Sulfuras, Hand of Ragnaros")
+        if(items[i].name != "Sulfuras, Hand of Ragnaros")
         {
             items[i].sellIn--;
         }
 
-        if (items[i].sellIn < 0)
+        if(items[i].sellIn < 0)
         {
-            if (items[i].name != "Aged Brie")
+            if(items[i].name != "Aged Brie")
             {
-                if (items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+                if(items[i].name != "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    if (items[i].quality > 0)
+                    if(items[i].quality > 0)
                     {
-                        if (items[i].name != "Sulfuras, Hand of Ragnaros")
+                        if(items[i].name != "Sulfuras, Hand of Ragnaros")
                         {
                             items[i].quality--;
                         }
@@ -69,7 +69,7 @@ void GildedRose::updateQuality()
                 }
                 else
                 {
-                    items[i].quality = items[i].quality - items[i].quality;
+                    items[i].quality--;
                 }
             }
             else
