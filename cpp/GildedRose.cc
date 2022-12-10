67b2a052d73555b86fe179aec8d2f0caf4b6a8ef
JayK669 <kkudanowski@gmail.com>
2022-12-10 18:40:55 +0100

    simplification of one if

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -6,51 +6,53 @@ GildedRose::GildedRose(::std::vector<Item> const& items) : items(items)
 
 GildedRose::GildedRose(::std::vector<Item> && items) : items(::std::move(items))
 {}
-
+auto brie = "Aged Brie";
+auto backstage = "Backstage passes to a TAFKAL80ETC concert";
+auto sulfuras = "Sulfuras, Hand of Ragnaros";
 void GildedRose::updateQuality()
 {
     for(int i = 0; i < items.size(); i++)
     {
-        if( items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" && items[i].name != "Sulfuras, Hand of Ragnaros")
-        {                        
-            if(items[i].quality > 0 )
-                    items[i].quality--;                          
+        if(items[i].name != brie && items[i].name != backstage && items[i].name != sulfuras && items[i].quality > 0)
+        {       
+            items[i].quality--;                 
         }
-        else if(items[i].quality != 0 && items[i].quality < 50 )
-        {           
+        else if(items[i].quality < 50 && items[i].quality > 0)
+        {
+            
             items[i].quality++;
 
-            if (items[i].name == "Backstage passes to a TAFKAL80ETC concert" && items[i].sellIn < 11 && items[i].quality < 50)
-            {                                      
-                items[i].quality++;     
-
-                if(items[i].sellIn < 6 && items[i].quality < 50)
-                {                      
-                    items[i].quality++;                       
+            if(items[i].name == backstage && items[i].sellIn < 11 ) 
+            {     
+                items[i].quality++;
+                if(items[i].sellIn < 6)
+                {
+                    items[i].quality++;
                 }
-            }           
+            }
+            
         }
 
-        if(items[i].name != "Sulfuras, Hand of Ragnaros")
+        if(items[i].name != sulfuras)
         {
             items[i].sellIn--;
         }
 
-        if(items[i].sellIn < 0 && items[i].name != "Aged Brie")
+        if(items[i].sellIn < 0 && items[i].name != brie)
         {
             
-            if(items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+            if(items[i].name != backstage)
             {                   
                 items[i].quality--;   
             }
             else
             {
-                items[i].quality -= items[i].quality;
-            }           
+                items[i].quality = 0;
+            }
         }
         else if(items[i].sellIn < 0 && items[i].quality < 50)
             {  
-                    items[i].quality++;                
+                items[i].quality++;                
             }
     }
 }
